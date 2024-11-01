a0ef32c57ec6663a778440359147fb4a8a9978f4
lokerklu <kluloker@gmail.com>
2024-11-01 14:27:24 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,33 @@
+import cors from "cors";
+import dotenv from "dotenv";
+import "./utils/database.js";
+import express from "express";
+import bodyParser from "body-parser";
+import router from "./routes/router.js"
+import cookieParser from "cookie-parser";
+import mongoSanitize from "express-mongo-sanitize";
+import rateLimitMiddleware from "./middleware/requestLimiter.js";
+
+dotenv.config();
+
+const port  = process.env.PORT || 8000;
+
+const app = express();
+
+app.use(cors({
+    origin: ["http://localhost:5173","https://lokerklu.info"], 
+    method: ["GET","POST"] 
+}))
+
+app.use(rateLimitMiddleware)
+app.use(cookieParser())
+app.use(express.json()) 
+app.use(express.static("public")) 
+app.use(bodyParser.urlencoded({extended: true})) 
+app.use(mongoSanitize())
+
+app.use(router);
+
+app.listen(port, () => {
+    console.log(`server still runing on port ${port}`);
+})
