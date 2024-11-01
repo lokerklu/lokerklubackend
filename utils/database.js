236a5a3267b0b6b011c7cb33168f1d40a765e8ee
lokerklu <kluloker@gmail.com>
2024-11-01 14:36:57 +0800

    Create database.js

---
+++ utils/database.js
@@ -0,0 +1,17 @@
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+
+dotenv.config()
+
+const database = process.env.MONGODB_URI
+
+mongoose.connect(database, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+const db = mongoose.connection;
+db.on("error", () => console.log("database tidak connect"))
+db.once("open", () => console.log("database connect"))
+
+export default mongoose
